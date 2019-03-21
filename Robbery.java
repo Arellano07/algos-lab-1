da5051dcda0b0429eeb5888d3c376d95f1bd7dbb
Arellano07 <48721921+Arellano07@users.noreply.github.com>
2019-03-21 00:22:48 -0400

    Update Robbery.java

--- Robbery.java
+++ Robbery.java
@@ -7,13 +7,32 @@
 public class Robbery {
 
 	// Using DP: Get the maximum value with capacity C and n items
+	public int max(
+		int a,
+		int b
+	) {
+		if(a>b){
+			return a;
+		}
+		return b;
+	}
+	
 	public int maximizeRobWorthRecur(
 		int capacity,
 		int[] sizes,
-		int[] worths
+		int[] worths,
+		int numOfItems
 	) {
 		// fill in here, change the return
+	
+		if(numOfItems==0||capacity==0){
 			return 0;
+		}
+		if(sizes[numOfItems-1]>capacity){
+			return maximizeRobWorthRecur(capacity,sizes,worths,numOfItems-1);
+		}
+		else return max(worths[numOfItems-1]+maximizeRobWorthRecur(capacity-sizes[numOfItems-1],sizes,worths,numOfItems-1),
+				maximizeRobWorthRecur(capacity,sizes,worths,numOfItems-1));
 	}
 
 	public int maximizeRobWorthBottomUp(
@@ -22,7 +41,24 @@ public class Robbery {
 		int[] worths
 	) {
 		// fill in here, change the return
-		return 0;
+		int i,j;
+		int numOfItems=worths.length;
+		int s[][]=new int [numOfItems+1][capacity+1];
+		
+		for(i=0;i<=numOfItems;i++){
+			for(j=0;j<=capacity;j++){
+			if(i==0||capacity==0){
+				s[i][j]=0;
+			}
+			else if (sizes[i-1]<=j){
+				s[i][j]=max(worths[i-1]+s[i-1][j-sizes[i-1]],s[i-1][j]);
+			}
+			else
+				s[i][j]=s[i-1][j];
+				
+			}
+		}
+		return s[numOfItems][capacity];
 	}
 
 /**
@@ -41,7 +77,7 @@ public class Robbery {
 		int[] itemSizes = {2, 25, 6, 13, 1, 15, 8, 5, 17, 4};
 		int[] itemWorths = {35, 120, 900, 344, 29, 64, 67, 95, 33, 10};
 
-		int maxWorthRecur = r.maximizeRobWorthRecur(bagCapacity, itemSizes, itemWorths);
+		int maxWorthRecur = r.maximizeRobWorthRecur(bagCapacity, itemSizes, itemWorths,itemWorths.length);
 		System.out.println("Max worth of the bag: " + maxWorthRecur);
 		int maxWorthBottomUp = r.maximizeRobWorthBottomUp(bagCapacity, itemSizes, itemWorths);
 		System.out.println("Max worth of the bag: " + maxWorthBottomUp);
